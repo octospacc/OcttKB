d5e041116504a32c74b8fa6e8344a4413d37609e
octt <6083316-octospacc@users.noreply.gitlab.com>
2022-09-22 15:38:33 +0000

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -10,5 +10,5 @@ tiddlywiki \
 	--rendertiddler $:/core/templates/static.template.css Style.css text/plain \
 	--rendertiddler $:/core/templates/alltiddlers.template.html Static.html text/plain
 
-wget -O "-1.html" "$URL" 2>/dev/null
-wget -O "-2.html" "$URL/-1.html" 2>/dev/null
+wget -O "-1.html" "$URL" || true
+wget -O "-2.html" "$URL/-1.html" || true
