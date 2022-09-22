10b10d7565fe7723e14ba310eab72ebd3858696b
octt <6083316-octospacc@users.noreply.gitlab.com>
2022-09-22 15:33:09 +0000

    Fix file; Update build script

--- Build.sh
+++ Build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+URL="https://kb.octt.eu.org"
+
 cd ./public
+
 tiddlywiki \
 	--load index.html \
 	--output ./ \
 	--rendertiddler $:/core/templates/static.template.css Style.css text/plain \
 	--rendertiddler $:/core/templates/alltiddlers.template.html Static.html text/plain
+
+wget -O "-1.html" "$URL"
+wget -O "-2.html" "$URL/-1.html"
