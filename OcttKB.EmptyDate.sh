7550ec9233f61a39c663400484dd129dbd99f20e
octt <6083316-octospacc@users.noreply.gitlab.com>
2023-03-07 14:50:47 +0000

    Update OcttKB.EmptyDate.sh

--- OcttKB.EmptyDate.sh
+++ OcttKB.EmptyDate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat << EOF > ./Wiki/tiddlers/\$__OcttKB_Empty.tid
+cat << EOF > ./Wiki/tiddlers/System/\$__OcttKB_Empty.tid
 created: 19700101000000000
 modified: $(date +%Y%m%d%H%M%S000)
 title: \$:/OcttKB/Empty
