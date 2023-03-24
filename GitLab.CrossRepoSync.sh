e0528394e6599acd238a71bdbf897d8b3e770b01
octt <6083316-octospacc@users.noreply.gitlab.com>
2023-03-24 11:38:21 +0000

    Update 2 files
    - /GitLab.CrossRepoSync.sh
    - /GitLab.Main.sh

--- GitLab.CrossRepoSync.sh
+++ GitLab.CrossRepoSync.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 TargetRepo="OcttKB.Web"
-cd ./Wiki
-tiddlywiki \
-	--verbose \
-	--output ./Output.tmp \
-	--rendertiddler "$:/core/save/all" "index.html" "text/plain"
-git clone --depth 1 https://gitlab.com/octtspacc/$TargetRepo ./$TargetRepo
-cd ./$TargetRepo
-cp -r ../Output.tmp/index.html ./public/index.html
+git clone --depth 1 https://gitlab.com/octtspacc/$TargetRepo ./Repo-$TargetRepo
+cd ./Repo-$TargetRepo
+cp -v ../Output.tmp/index.html ./public/index.html
 git add .
 git commit -m "OcttKB Cross-Repo Sync (Raw to HTML)"
 git push
--- GitLab.Main.sh
+++ GitLab.Main.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 sh ./OcttKB.EmptyDate.sh
 sh ./SetupGitProfile.sh
+
+tiddlywiki \
+	./Wiki \
+	--verbose \
+	--output ./Output.tmp \
+	--rendertiddler "$:/core/save/all" "index.html" "text/plain"
+
+mkdir -vp ./public
+cp -v ./Output.tmp/index.html ./public/index.html
+
 CommitCheck="$(git log -1 --pretty=%B)"
 CommitMsg0="OcttKB Cross-Repo Sync"
 [ "$CommitCheck" != "$CommitMsg0 (HTML to Raw)" ] && [ "$CommitCheck" != "$CommitMsg0 (Raw to HTML)" ] && sh ./GitLab.CrossRepoSync.sh || true
