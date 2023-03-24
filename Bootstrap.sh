a9006db2dc54e8db22547493d35985db9950316c
octt <6083316-octospacc@users.noreply.gitlab.com>
2023-03-24 14:45:03 +0000

    Update 3 files
    - /Wiki-OcttKB/tiddlers/System/$__OcttKB_Repo_GitLab.Main.sh.txt
    - /Wiki-OcttKB/tiddlers/System/$__OcttKB_Repo_GitLab.Main.sh.txt.meta
    - /Bootstrap.sh

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+# Export all tiddlers from the specific path of the HTML wiki
+tiddlywiki \
+	--verbose \
+	--load ./public/index.html \
+	--output ./Output.tmp \
+	--save "[prefix[$:/OcttKB/Repo/]]"
+
+# Move the exported folder to a better location
+mv ./Output.tmp/\$_/OcttKB/Repo ./Repo.tmp
+
+# Rename all extracted file to have a correct extension (remove forced .txt suffix)
+# Don't filter for just .sh files anymore as we have other kinds of files
+cd ./Repo.tmp
+for File in *.txt
+do
+	mv "$File" "${File/.txt}"
+done
+cd ..
+
+# Move the files of this repo to overwrite the extracted ones, then move everything back
+# This is so, if present, files from the repo are preferred, if needed in case of emergency
+mv ./Repo/* ./Repo.tmp/
+mv ./Repo.tmp/* ./Repo/
+
+# Move everything to the working directory, ready for the next CI steps
+mv ./Repo/* ./
+chmod +x *.sh *.py || true
